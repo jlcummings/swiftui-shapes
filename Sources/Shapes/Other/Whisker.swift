fc0cff295e476ddd266c32d3b63b11b34e7bea02
Justin L. Cummings <666404+jlcummings@users.noreply.github.com>
2021-10-31 13:13:50 -0500

    Add a composable "whisker" shape that be added to other shapes, such as with a rectangle or capsule to create a box-and-whiskers object.

---
+++ Sources/Shapes/Other/Whisker.swift
@@ -0,0 +1,70 @@
+//
+//  Candlestick.swift
+//  
+//
+//  Created by Justin Cummings on 10/30/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Whisker: View, InsettableShape {
+    private let inset: CGFloat
+
+    public func inset(by amount: CGFloat) -> Whisker {
+        Whisker(inset: self.inset + amount)
+    }
+
+    public var body: some View {
+        GeometryReader { geometry in
+            self.path(in: CGRect(x: 0, y: 0, width: geometry.size.width, height: geometry.size.height))
+        }
+    }
+
+    func midpoint(of size: CGFloat) -> CGFloat {
+        return size / 2
+    }
+
+    public let thickness: CGFloat = 2
+
+    public init(inset: CGFloat = 0) {
+        self.inset = inset
+    }
+
+    func path(in rect: CGRect) -> Path {
+        let width = rect.size.width - inset * 2
+        let height = rect.size.height - inset * 2
+        let horizontalCenter = midpoint(of: width)
+        let thicknessOffset = midpoint(of: thickness)
+        return Path { path in
+
+            // start at top left corner of whisker oriented vertically with whisker ends at bottom
+            path.move(to: CGPoint(x: horizontalCenter - thicknessOffset, y: 0))
+            // left edge of vertical part
+            path.addLine(to: CGPoint(x: horizontalCenter - thicknessOffset, y: height - thickness))
+            // top left edge of whisker end
+            path.addLine(to: CGPoint(x: 0, y: height - thickness))
+            // vertical edge of left whisker end
+            path.addLine(to: CGPoint(x: 0, y: height))
+            // bottom edge of whisker end
+            path.addLine(to: CGPoint(x: width, y: height))
+            // vertical edge of right whisker end
+            path.addLine(to: CGPoint(x: width, y: height - thickness))
+            // top edge of right whisker end
+            path.addLine(to: CGPoint(x: horizontalCenter + thicknessOffset, y: height - thickness))
+            // right edge of vertical part
+            path.addLine(to: CGPoint(x: horizontalCenter + thicknessOffset, y: 0))
+            // top edge of vertical part
+            path.closeSubpath()
+        }.offsetBy(dx: inset, dy: inset)
+
+    }
+
+}
+
+struct Whisker_Previews: PreviewProvider {
+    static var previews: some View {
+        Whisker()
+            .previewLayout(.fixed(width: 200, height: 300))
+    }
+}
